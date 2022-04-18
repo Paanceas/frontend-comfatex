53ea5c90292e6f89ca5e2cdb853a9acdab493cdd
Pablo Ceballos <pablo-s_972011@hotmail.com>
2022-04-17 22:04:46 -0500

    ✨ logica inicial de la tienda

--- src/app/modules/page/components/details-product/details-product.component.ts
+++ src/app/modules/page/components/details-product/details-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute,Params, Router} from "@angular/router";
+import * as productos from '../../../../common/productos.json';
 
 @Component({
   selector: 'app-details-product',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _activeRouter: ActivatedRoute, private router: Router) {}
+
+  private products:any[] = productos.default;
+
+  producto:any;
+  relacionados:any[] = [];
 
   ngOnInit(): void {
+    this._activeRouter.params.subscribe((params: any) => {
+      if(params && params.item){
+        this.relacionados = [];
+        this.producto = this.products.find( item => item.id == params.item);
+        if(this.producto){
+          this.productsRelated();
+        }else{
+          this.router.navigate(['/page/tienda']);
+        }
+      }else{
+        this.router.navigate(['/page/tienda']);
+      }
+    });
   }
 
+  private productsRelated(){
+    while (this.relacionados.length < 3) {
+      const selected = this.products[Math.floor(Math.random() * this.products.length)];
+      if(selected.id != this.producto.id){
+        this.relacionados.push(selected);
+        const filteredArray = this.relacionados.filter( (ele,pos)=> this.relacionados.indexOf(ele) == pos);
+        this.relacionados = filteredArray;
+      }
+    }
+  }
 }
--- src/app/modules/page/page.module.ts
+++ src/app/modules/page/page.module.ts
@@ -5,12 +5,14 @@ import { PageRoutingModule } from './page-routing.module';
 import { GlobalModule } from 'src/app/components/components.module';
 import { StoreComponent } from './components/store/store.component';
 import { DetailsProductComponent } from './components/details-product/details-product.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     InitialComponent,
     StoreComponent,
-    DetailsProductComponent
+    DetailsProductComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
